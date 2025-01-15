e2fd7043e4fbbbadb6fb6f4739b7c5d24d3decb7
dinara-t <dinara.tiumina@gmail.com>
2025-01-14 23:57:16 -0500

    initial commit

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:d27hnBF3QyWEtdhLRo5ZqKnI@172.21.217.185:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
